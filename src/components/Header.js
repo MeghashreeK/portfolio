0de8a501ffa72e5af59a8065898fb2aca5ca5e05
MeghashreeK <kundermeghashree279@gmail.com>
2024-07-02 17:09:13 +0530

    changed text size of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -34,7 +34,7 @@ const Header = () => {
 
   return (
     <div className='text-white bg-[#010B13] py-2 px-4'>
-      <div className="flex justify-between sm:justify-evenly w-full fixed bg-[#1E293B] rounded-lg p-2">
+      <div className="flex justify-between sm:justify-evenly w-full fixed bg-[#1E293B] rounded-lg p-2 text-[14px] sm:text-[16px]">
         {headerArray.map((headerValue,index) => <p className={`cursor-pointer ${borderState[index] ? "border-b-2 border-yellow-400 text-yellow-400" : ""}`} key={headerValue.text} onClick={() =>handleScrollEvent(index)}>{headerValue.text}</p>)}
       </div>
       <div ref={aboutMe}><AboutMe /></div>
