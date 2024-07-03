71d1f63c4a10212cadd6175b23c0b2c56cd811b2
MeghashreeK <kundermeghashree279@gmail.com>
2024-07-03 12:09:57 +0530

    added about me details

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,9 +5,10 @@ const AboutMe = () => {
     return (
         <div className="flex flex-col items-center justify-center h-screen gap-5 text-center">
             <h1 className='text-4xl font-bold font-Merriweather'>About me</h1>
-            <p className='w-1/2 text-[14px] sm:text-[16px] font-Montserrat'>
-                With it installed in the code editor you are using, you can type “lorem” and then tab and it will expand into a paragraph of Lorem Ipsum placeholder text.
-                But it can do more! You can control how much you get, place it within HTML structure as it expands, and get different bits of it in repeated elements.
+            <p className='w-full sm:w-1/2 text-[14px] sm:text-[16px] font-Montserrat'>
+            Hello, I'm Meghashree Kunder, a passionate Frontend Web Developer and a 2023 BSc Computer Science graduate. For me, creating websites isn't just a career choice; it's a true passion. 
+            I love building websites that are not only functional but also well designed and fully responsive.
+            Currently, I create websites using ReactJS, TailwindCSS, and Redux. The knowledge required for web development is like a vast ocean, and I am very much excited to dive deep and learn new technologies.
             </p>
         </div>
     )
