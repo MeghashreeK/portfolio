252506c45bdad1e18d5581415b090fdfb342ac49
MeghashreeK <kundermeghashree279@gmail.com>
2024-07-03 12:44:37 +0530

    changed about me 'I believe'

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,7 +7,7 @@ const AboutMe = () => {
             <p className='w-full sm:w-1/2 text-[14px] sm:text-[16px] font-Montserrat'>
             Hello, I'm Meghashree Kunder, a passionate Frontend Web Developer and a 2023 BSc Computer Science graduate. For me, creating websites isn't just a career choice; it's a true passion. 
             I love building websites that are not only functional but also well designed and fully responsive.
-            Currently, I create websites using ReactJS, TailwindCSS, and Redux. The knowledge required for web development is like a vast ocean, and I am very much excited to dive deep and learn new technologies.
+            Currently, I create websites using ReactJS, TailwindCSS, and Redux. I believe the knowledge required for web development is like a vast ocean, and I am very much excited to dive deep and learn new technologies.
             </p>
         </div>
     )
